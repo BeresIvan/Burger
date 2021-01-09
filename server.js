74c903d72fbbebb55dc94b98a007a653de14df23
GTIvan <ivberes@gmail.com>
2021-01-09 17:39:52 -0600

    added the handlebars and css files, also added the images

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
+// Pull in required dependencies
+let express = require('express');
+let bodyParser = require('body-parser');
+let methodOverride = require('method-override');
 
 let port = process.env.PORT || 3000;
 
 let app = express();
+
+// Serve static content for the app from the 'public' directory
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+// Set Handlebars as the view engine
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+console.log(template({ name: "Nils" }));
+
+// Import routes and give the server access to them
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+app.listen(port);
