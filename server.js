23b12fd8a579840e9f1fddd00c0f32daf8b9ee1b
GTIvan <ivberes@gmail.com>
2021-01-10 15:54:17 -0600

    fixes

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-// Pull in required dependencies
-let express = require('express');
-let bodyParser = require('body-parser');
-let methodOverride = require('method-override');
-
-let port = process.env.PORT || 8080;
-
-
-let app = express();
-
-// Serve static content for the app from the 'public' directory
-app.use(express.static(process.cwd() + '/public'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
-
-// Set Handlebars as the view engine
+// Create express connection and run node server
+var express = require("express");
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+var methodOverride = require("method-override");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+// This is a level of abstraction to hide credentials from user
+app.use(express.static('public'));
+
+// Parse application body as JSON
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(express.json());
+
+// the main page is always displayed
+app.use(methodOverride('_method'));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
+// Norm is to call it "var = routes"
+var routes = require("./controllers/burger_controller.js");
 
-let routes = require('./controllers/burger_controller.js');
+// Use express routes defined
+app.use("/", routes);
 
-app.use('/', routes);
-
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
