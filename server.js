e11ce965cc6599f29d3b469592a13cf4842807c0
GTIvan <ivberes@gmail.com>
2021-01-12 18:17:27 -0600

    typo fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 // This is a level of abstraction to hide credentials from user
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
 // Parse application body as JSON
 app.use(express.urlencoded({
